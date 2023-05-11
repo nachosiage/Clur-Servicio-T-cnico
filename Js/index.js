54736fb07af6d1d91bf93fea865f1571ce7ed815
Juan Ignacio Siage <nachosiage@users.noreply.github.com>
2023-05-11 00:10:22 -0300

    v-0.0.1

---
+++ Js/index.js
@@ -0,0 +1,13 @@
+// MEDIA QUERYS
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
